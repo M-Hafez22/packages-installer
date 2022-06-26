d0507979af25468efb4ef10cc1064755418b5f27
M-Hafez22 <mohamedhafezmansour@gmail.com>
2022-06-27 00:04:53 +0200

    Add repos questions

--- fedora-package-installer.sh
+++ fedora-package-installer.sh
@@ -9,5 +9,17 @@ for QUESTION in $QUESTIONS
     read -p "Do you want to $QUESTION❔ y/n: " $QUESTION
 	done
 
+# A.2. Add ADD_REPOSITORIES
+case "$ADD_REPOSITORIES" in [yY] | [yY][eE][sS])
+    REPOS="FUSION SANP FLATPAK"
+    for REPO in $REPOS
+        do
+        read -p "Do you want to Add $REPO Repositories❔ y/n: " $REPO
+        done
+    ;;
+    *)
+    printf ""
+    ;;
+esac
 
 # B. Process

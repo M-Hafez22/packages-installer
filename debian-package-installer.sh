3c4783fc5261cd784fffd5fd3ae0f7abdcc3cc56
M-Hafez22 <mohamedhafezmansour@gmail.com>
2022-06-27 00:31:30 +0200

    Ask main questions

--- debian-package-installer.sh
+++ debian-package-installer.sh
@@ -1,3 +1,10 @@
 #! /usr/bin/bash
 
 printf "\n\e[1;31m Welcome To Debian \e[0m\n\n"
+
+# A. Ask
+QUESTIONS="UPDATE ADD_REPOSITORIES INSTALL_PACKAGES"
+for QUESTION in $QUESTIONS
+	do
+    read -p "Do you want to $QUESTION❔ y/n: " $QUESTION
+done

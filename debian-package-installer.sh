a25eaeb1896082b434673af2fb8414acbb75b0db
M-Hafez22 <mohamedhafezmansour@gmail.com>
2022-06-27 00:39:07 +0200

    Add repos

--- debian-package-installer.sh
+++ debian-package-installer.sh
@@ -9,6 +9,19 @@ for QUESTION in $QUESTIONS
     read -p "Do you want to $QUESTION❔ y/n: " $QUESTION
 done
 
+# A.2. Add ADD_REPOSITORIES
+case "$ADD_REPOSITORIES" in [yY] | [yY][eE][sS])
+    REPOS="FUSION SANP FLATPAK"
+    for REPO in $REPOS
+        do
+        read -p "Do you want to Add $REPO Repositories❔ y/n: " $REPO
+        done
+    ;;
+    *)
+    printf ""
+    ;;
+esac
+
 # B. Process
 
 # Install Package function
@@ -33,4 +46,26 @@ case "$UPDATE" in [yY] | [yY][eE][sS])
     *)
     printf ""
     ;;
+esac
+
+# B.2. ADD_REPOSITORIES
+# Snap
+case "$SANP" in [yY] | [yY][eE][sS])
+  printf "\n\n📥\e[1;32m Adding Snap\e[0m\n"
+  sudo apt update &&  sudo apt install -y  snapd &&  sudo snap install core &&  sudo snap install hello-world &&  sudo apt-get update -y  &&  sudo apt-get install -y gnome-software-plugin-snap
+  printf "\n✅ \e[1;32m Reboot to use Sanp\e[0m\n"
+  ;;
+  *)
+  printf ""
+  ;;
+esac
+# Flatpak
+case "$FLATPAK" in [yY] | [yY][eE][sS])
+  printf "\n\n📥\e[1;32m  Adding Flatpak\e[0m\n"
+  sudo apt install flatpak && sudo apt install gnome-software-plugin-flatpak  && flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+  printf "\n✅\e[1;32m flatpak\e[0m\n"
+  ;;
+  *)
+  printf ""
+  ;;
 esac

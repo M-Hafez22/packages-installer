e564713ade5652b304bc9fb3ff8650602f1d81e6
M-Hafez22 <mohamedhafezmansour@gmail.com>
2022-06-27 00:36:16 +0200

    Add install function

--- debian-package-installer.sh
+++ debian-package-installer.sh
@@ -8,3 +8,18 @@ for QUESTION in $QUESTIONS
 	do
     read -p "Do you want to $QUESTION❔ y/n: " $QUESTION
 done
+
+# B. Process
+
+# Install Package function
+function installPackage() {
+    case "$1" in [yY] | [yY][eE][sS])
+        printf "\n\n📥\e[1;32m  Installing $2\e[0m\n"
+        sudo apt install -y $2
+        printf "\n✅\e[1;32m  $2\e[0m\n"
+        ;;
+        *)
+        printf ""
+        ;;
+    esac
+}

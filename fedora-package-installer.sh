d717539d8190c685bc36d86751516b23261ac7a2
M-Hafez22 <mohamedhafezmansour@gmail.com>
2022-06-27 00:04:12 +0200

    Add main questions

--- fedora-package-installer.sh
+++ fedora-package-installer.sh
@@ -3,5 +3,11 @@
 printf "\n\e[1;31m Welcome To Fedora \e[0m\n\n"
 
 # A. Ask
+QUESTIONS="UPDATE ADD_REPOSITORIES INSTALL_PACKAGES"
+for QUESTION in $QUESTIONS
+	do
+    read -p "Do you want to $QUESTION❔ y/n: " $QUESTION
+	done
+
 
 # B. Process

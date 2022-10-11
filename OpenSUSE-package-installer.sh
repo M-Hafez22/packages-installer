2cf0c27b76a2ec2ce5244f80317c386de016a3ad
Mohamed Hafez <mohamedhafezmansour@gmail.com>
2022-10-11 15:47:46 +0200

    Add installing Functions

--- OpenSUSE-package-installer.sh
+++ OpenSUSE-package-installer.sh
@@ -2,3 +2,46 @@
 
 printf "\n\e[1;31m Welcome To OpenSUSE Tumbleweed \e[0m\n\n"
 
+# Functions
+# Ask For Options
+function optionsList() {
+  case "$1" in [yY] | [yY][eE][sS])
+    printf "\n\e[1;32m $2: \e[0m\n\n"
+    options="$3"
+    counter=1
+    for option in $options; do
+      read -p "$counter. Do you want to $option ❔ y/n: " $option
+      ((counter++))
+    done
+    ;;
+  *)
+    printf ""
+    ;;
+  esac
+}
+
+# Install Package With Zypper
+function installPackage() {
+  case "$1" in [yY] | [yY][eE][sS])
+    printf "\n\n📥\e[1;32m  Installing $2\e[0m\n"
+    sudo zypper install -y $2
+    printf "\n✅\e[1;32m  $2\e[0m\n"
+    ;;
+  *)
+    printf ""
+    ;;
+  esac
+}
+
+# Install Package With Flatpack
+function installPackageFlatpack() {
+  case "$1" in [yY] | [yY][eE][sS])
+    printf "\n\n📥\e[1;32m  Installing $2\e[0m\n"
+    flatpak install -y flathub $2
+    printf "\n✅\e[1;32m  $2\e[0m\n"
+    ;;
+  *)
+    printf ""
+    ;;
+  esac
+}
